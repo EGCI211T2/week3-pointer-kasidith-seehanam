074ae1d1b930d7cba4bac22d2aa4e9d6296620df
kasidith-seehanam <kasidith.see@student.mahidol.edu>
2025-10-03 03:59:39 +0000

    leo

--- main.cpp
+++ main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10
+
 
 using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
+ int n =argc-1;
+
+  pa=new int[n];
+
+  for(i=0; i<n; i++){
+
+   // cout<<"Input no."<<i<<":";
+
+    //cin>> *pa;
+
+    *pa=atoi(argv[i+1]);
+
+    pa++;
+
+  }
+cout <<"How many numbers?";
+cin >> n;
+pa = new int[n];
+pb = new int[n];
+ 
+for(i=0; i<n; i++)
+{
+cout << "Input "<< i+1 <<" :";
+cin >> *pa;
+pa++;
+}
+pa-=n;
+
 cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+  for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+  }
+  cout<<setw(3)<<*pa<<endl;
+  pb=pa;
+  pa -= (n-1);
 
+  for (i=0; i<(n/2); i++){
+    temp = *pa;  
+   *pa = *pb;  
+   *pb = temp;
+   pa++;  pb--;
+  }
+  pa -= (n/2);
+
+/*
 pa = &a[0];  
 pb = &a[SIZE-1];
 for (i=0; i<SIZE/2; i++)
@@ -26,13 +63,14 @@ for (i=0; i<SIZE/2; i++)
 }
 pa=&a[0];
 pb-=SIZE/2;
+*/
 cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
 
-
+delete []pa;
 return 0;
 }
